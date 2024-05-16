124c53f43f95412490e80152ff299cb969a62191
YHassan3 <yh@nexcomglobal.com>
2024-05-16 15:35:54 +0100

    initial speech to text works

---
+++ text_to_speech.py
@@ -0,0 +1,18 @@
+from ibm_watson import TextToSpeechV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+authenticator = IAMAuthenticator('M3wUUJKQxq_LJ_vDEfvUhDZewwyJyFqoO1STfdqk9SAF')
+text_to_speech = TextToSpeechV1(
+    authenticator=authenticator
+)
+
+text_to_speech.set_service_url('https://api.au-syd.text-to-speech.watson.cloud.ibm.com/instances/0ca23754-b89a-4029-85cc-0131bae73271')
+text_to_speech.set_disable_ssl_verification(True)
+
+with open('hello_world.wav', 'wb') as audio_file:
+    audio_file.write(
+        text_to_speech.synthesize(
+            'Martin stop working too late, you need to take care of your health',
+            voice='en-US_AllisonV3Voice',
+            accept='audio/wav'        
+        ).get_result().content)
