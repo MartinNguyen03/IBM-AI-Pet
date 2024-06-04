089314e00b7faa8855f912516d35285072e0bbcc
Martin Nguyen <martinguyen2003@gmail.com>
2024-06-04 16:01:53 +0100

    connectdb

--- server/server.js
+++ server/server.js
@@ -2,26 +2,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
+const connectDB = require('./db/db.js');
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected...');
-  } catch (err) {
-    console.log('MongoDB connection failed...');
-    console.error(err.message);
-    process.exit(1);
-  }
-};
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('MongoDB connected...');
+//   } catch (err) {
+//     console.log('MongoDB connection failed...');
+//     console.error(err.message);
+//     process.exit(1);
+//   }
+// };
 
 connectDB();
 
