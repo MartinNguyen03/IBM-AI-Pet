4e07c564f8c787133a5cffea3d4af262a9d3f882
Jerry7234234 <134069780+Jerry7234234@users.noreply.github.com>
2024-06-20 21:58:37 +0100

    Renamed some files

--- display/emotion.py
+++ display/emotion.py
@@ -1,9 +1,9 @@
+import pygame, sys, os, cv2
 from os import environ
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
-import pygame, sys, os
 from IPython.display import clear_output
 
-class Bot(pygame.sprite.Sprite):
+class BotEmotion(pygame.sprite.Sprite):
 	def __init__(self, pos_x, pos_y):
 		super().__init__()
 		self.happy = False
@@ -21,17 +21,17 @@ class Bot(pygame.sprite.Sprite):
 
 		print('Loading assets...')
 		for i in range(1, 104):
-			self.sprites_idle.append(pygame.image.load(os.path.join(r'C:\Users\Jerry\OneDrive\Documents\Imperial\3rd year\3rd year Group project\Image\v1\idle', str(i) + '.png')))
-		for i in range(6, 85):
-			self.sprites_happy.append(pygame.image.load(os.path.join(r'C:\Users\Jerry\OneDrive\Documents\Imperial\3rd year\3rd year Group project\Image\v1\happy', str(i) + '.png')))
-		for i in range(1, 51):
-			self.sprites_sad.append(pygame.image.load(os.path.join(r'C:\Users\Jerry\OneDrive\Documents\Imperial\3rd year\3rd year Group project\Image\v1\sad', str(i) + '.png')))
-		for i in range(16, 73):
-			self.sprites_bored.append(pygame.image.load(os.path.join(r'C:\Users\Jerry\OneDrive\Documents\Imperial\3rd year\3rd year Group project\Image\v1\bored', str(i) + '.png')))
-		for i in range(10, 82):
-			self.sprites_angry.append(pygame.image.load(os.path.join(r'C:\Users\Jerry\OneDrive\Documents\Imperial\3rd year\3rd year Group project\Image\v1\angry', str(i) + '.png')))
-		for i in range(10, 106):
-			self.sprites_love.append(pygame.image.load(os.path.join(r'C:\Users\Jerry\OneDrive\Documents\Imperial\3rd year\3rd year Group project\Image\v1\love', str(i) + '.png')))
+			self.sprites_idle.append(pygame.image.load(os.path.join(r'C:\Users\Jerry\OneDrive\Documents\Imperial\3rd year\3rd year Group project\Animation\v1\idle', str(i) + '.png')))
+		for i in range(7, 105):
+			self.sprites_happy.append(pygame.image.load(os.path.join(r'C:\Users\Jerry\OneDrive\Documents\Imperial\3rd year\3rd year Group project\Animation\v1\happy', str(i) + '.png')))
+		for i in range(1, 62):
+			self.sprites_sad.append(pygame.image.load(os.path.join(r'C:\Users\Jerry\OneDrive\Documents\Imperial\3rd year\3rd year Group project\Animation\v1\sad', str(i) + '.png')))
+		for i in range(33, 63):
+			self.sprites_bored.append(pygame.image.load(os.path.join(r'C:\Users\Jerry\OneDrive\Documents\Imperial\3rd year\3rd year Group project\Animation\v1\bored', str(i) + '.png')))
+		for i in range(11, 91):
+			self.sprites_angry.append(pygame.image.load(os.path.join(r'C:\Users\Jerry\OneDrive\Documents\Imperial\3rd year\3rd year Group project\Animation\v1\angry', str(i) + '.png')))
+		for i in range(10, 122):
+			self.sprites_love.append(pygame.image.load(os.path.join(r'C:\Users\Jerry\OneDrive\Documents\Imperial\3rd year\3rd year Group project\Animation\v1\love', str(i) + '.png')))
 		print('Done')
 
 		self.t = 0
@@ -97,13 +97,37 @@ class Bot(pygame.sprite.Sprite):
 
 			self.image = self.sprites_idle[int(self.t_idle)]
 
+def get_snapshots(video_dir, image_dir, skip):
+	capture = cv2.VideoCapture(video_dir)
+	i = 0
+	j = 1
+	while True:
+		cont, frame = capture.read()
+		if cont:
+			if j % skip == 0:
+				frame = cv2.resize(frame, [960, 540])
+				mask = cv2.inRange(frame[..., 1], 180, 255.)
+				frame[..., 0][np.where(mask!=0)] = 255.
+				frame[..., 0][np.where(mask==0)] = 0.
+				frame[..., 1][np.where(mask!=0)] = 255.
+				frame[..., 1][np.where(mask==0)] = 0.
+				frame[..., 2][np.where(mask!=0)] = 0.
+				# frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+				cv2.imwrite(os.path.join(image_dir, str(i + 1) + '.png'), frame)
+				print('Captured ' + str(i + 1) + ' images.')
+				i += 1
+			cv2.waitKey(1)
+		else:
+			break
+		j += 1
+
 if __name__ == '__main__':
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     pygame.display.set_caption("Sprite Animation")
     moving_sprites = pygame.sprite.Group()
-    bot = Bot(150, 100)
+    bot = BotEmotion(150, 100)
     moving_sprites.add(bot)
 
     while True:
