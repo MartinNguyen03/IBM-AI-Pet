7e60f39395fcbde082936c6d6bd0cf436f375bca
Martin Nguyen <martinguyen2003@gmail.com>
2024-06-21 03:10:06 +0100

    made chatbot less buggy with google search

--- watson_assistant/watson.py
+++ watson_assistant/watson.py
@@ -37,7 +37,7 @@ class WatsonAssistant:
             ).get_result()
             self.session_id = None
     
-    def handle_chat(self, user_input, max_retries=4):
+    def handle_chat(self, user_input, max_retries=5):
         if self.session_id is None:
             self.create_session()
         
@@ -74,43 +74,26 @@ class WatsonAssistant:
         
         return message_output
 
-def chatbot():
-    api_key = 'CNMroTYvvNhmlODBsgfGDXt7oDU-_83_-4KoMm6elTRG'
-    service_url = 'https://api.au-syd.assistant.watson.cloud.ibm.com/instances/698ca409-f562-471e-a74b-a2efdd5e3259'
-    assistant_id = '57bdddd6-b3a3-452c-becd-a8b3ed689e9d'
 
-    watsonAssistant = WatsonAssistant(api_key, service_url, assistant_id)
+api_key = 'CNMroTYvvNhmlODBsgfGDXt7oDU-_83_-4KoMm6elTRG'
+service_url = 'https://api.au-syd.assistant.watson.cloud.ibm.com/instances/698ca409-f562-471e-a74b-a2efdd5e3259'
+assistant_id = '57bdddd6-b3a3-452c-becd-a8b3ed689e9d'
 
-    # Example of multi-turn conversation
-    try:
-        while True:
-            user_input = speechToText()
-            if user_input.lower() == "exit":
-                break
-            response = watsonAssistant.handle_chat(user_input)
+watsonAssistant = WatsonAssistant(api_key, service_url, assistant_id)
+
+# Example of multi-turn conversation
+try:
+    while True:
+        user_input = speechToText()
+        if user_input.lower() == "exit":
+            break
+        response = watsonAssistant.handle_chat(user_input)
+        print(f"Watson Response: {response}")
+        text_to_speech(response)
+        if 'search for' in response.lower():
+            response = watsonAssistant.handle_chat("continue search")
             print(f"Watson Response: {response}")
             text_to_speech(response)
-    finally:
-        # Properly close the session when done
-        watsonAssistant.delete_session()
-
-# For text-based chatbot
-def textbot():
-    api_key = 'CNMroTYvvNhmlODBsgfGDXt7oDU-_83_-4KoMm6elTRG'
-    service_url = 'https://api.au-syd.assistant.watson.cloud.ibm.com/instances/698ca409-f562-471e-a74b-a2efdd5e3259'
-    assistant_id = '57bdddd6-b3a3-452c-becd-a8b3ed689e9d'
-
-    watsonAssistant = WatsonAssistant(api_key, service_url, assistant_id)
-
-    try:
-        while True:
-            user_input = input("You: ")
-            if user_input.lower() == "exit":
-                break
-            response = watsonAssistant.handle_chat(user_input)
-            print(f"Watson Response: {response}")
-    finally:
-        # Properly close the session when done
-        watsonAssistant.delete_session()
-
-chatbot()
+finally:
+    # Properly close the session when done
+    watsonAssistant.delete_session()
