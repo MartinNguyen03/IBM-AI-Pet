eba14e03c988930ee75b5f93c449a9541aea00d6
Jerry7234234 <134069780+Jerry7234234@users.noreply.github.com>
2024-06-28 22:22:08 +0100

    Update face.py

--- athena/face.py
+++ athena/face.py
@@ -63,22 +63,58 @@ def modify_model(model):
 
     return model
 
+# Define button class
+class Button:
+    def __init__(self, text, x, y, width, height):
+        self.text = text
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.action = action
+        self.color = (200, 200, 200)
+        self.text_color = (0, 0, 0)
+
+    def draw(self, img):
+        cv2.rectangle(img, (self.x, self.y), (self.x + self.width, self.y + self.height), self.color, -1)
+        cv2.putText(img, self.text, (self.x + 10, self.y + self.height // 2), cv2.FONT_HERSHEY_SIMPLEX, 0.7, self.text_color, 2)
+
+    def is_pressed(self, x, y):
+        return self.x < x < self.x + self.width and self.y < y < self.y + self.height
+
+global take_pic, cancel
+globals()['take_pic'] = False
+globals()['cancel'] = False
+
+# Mouse callback function
+def mouse_callback(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        for button in buttons:
+            if button_ok.is_pressed:
+		globals()['take_pic'] = True
+	    if button_cancel.is_pressed:
+		globals()['cancel'] = True
+		    
 def remember(video_capture, model):
+	# Initialize buttons
+	button_ok = Button("Button 1", 50, 50, 200, 100, button_action)
+	button_cancel = Button("Button 2", 50, 200, 200, 100, button_action)
 	num = open(r'C:\Users\Jerry\OneDrive\Documents\Imperial\3rd year\3rd year Group project\Train data\known people.txt').read()
 	roi_x, roi_y, roi_width, roi_height = 150, 40, 300, 440
 	cv2.namedWindow("Test", cv2.WINDOW_NORMAL)
 	cv2.setWindowProperty("Test", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+	cv2.setMouseCallback("Touch Screen Interface", mouse_callback)
 	not_done = True
 	while not_done:
 		success, frame = video_capture.read()  # read frames from the video
 		if not success:
 			break
 		cv2.putText(frame, "Please make sure that your face fits inside the rectangle", (10, 30), cv2.LINE_AA, fontScale = 0.6, color = (255, 0, 0))
-		cv2.putText(frame, "When you're ready, press z", (10, 60), cv2.LINE_AA, fontScale = 0.6, color = (255, 0, 0))
+		cv2.putText(frame, "Press 'OK' to take pictures; press 'Cancel' to cancel", (10, 60), cv2.LINE_AA, fontScale = 0.6, color = (255, 0, 0))
 		cv2.rectangle(frame, (roi_x, roi_y), (roi_x + roi_width, roi_y + roi_height), (0, 255, 0), 2)
 		cv2.imshow("Test", frame)
 
-		if cv2.waitKey(1) & 0xFF == ord("z"):
+		if cv2.waitKey(1) and globals['take_pic']:
 			os.mkdir(os.path.join(r'C:\Users\Jerry\OneDrive\Documents\Imperial\3rd year\3rd year Group project\Train data\Faces', 'z ' + num))
 			dir_save = os.path.join(r'C:\Users\Jerry\OneDrive\Documents\Imperial\3rd year\3rd year Group project\Train data\Faces', 'z ' + num)
 			for i in range(400):
@@ -106,7 +142,7 @@ def remember(video_capture, model):
 			model = new_model
 			not_done = False
 
-		if cv2.waitKey(1) & 0xFF == ord("q"):
+		if cv2.waitKey(1) and globals(0['cancel']:
 			not_done = False
 
 	cv2.destroyWindow("Test")
